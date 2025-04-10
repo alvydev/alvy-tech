a63f32fe72f45e587b98068a9fe2a9e6f2d7ee42
Daniel <dannyalvy05@gmail.com>
2025-04-10 11:48:49 -0700

    Added main content

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,40 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Home = () => {
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex items-center justify-center relative"
+    >
+      <RevealOnScroll>
+        <div className="text-center z-10 px-4">
+          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
+            Hi, I'm Alvy
+          </h1>
+
+          <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
+            I'm a full-stack software engineer specializing in scalable, 
+            cloud-based web applications with expertise in ReactJS, AngularJS,
+            Node.js, Python, C# .NET Core, Java Springboot, PostgreSQL and Ms SQL Server.
+          </p>
+          <div className="flex justify-center space-x-4">
+            <a
+              href="#about"
+              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+            >
+              About Me
+            </a>
+
+            <a
+              href="#contact"
+              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
+             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
+            >
+              Contact Me
+            </a>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,22 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Featured Projects
+          </h2>
+          <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
+            Coming Soon
+          </p>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
